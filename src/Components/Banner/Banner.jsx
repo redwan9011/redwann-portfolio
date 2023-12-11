5f4929345a85632c07af95fd0a84f454ec7f734d
redwan9011 <redwan390309@gmail.com>
2023-12-11 20:50:56 +0600

    set the resume download system

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,6 +1,6 @@
 import image from '../../assets/1702274669682__3333_-removebg-preview (1).png'
 import { FaLinkedin,FaGithub } from "react-icons/fa";
-
+import pdf from '../../assets/questions (1).pdf'
 const Banner = () => {
     return (
   <div className=''>
@@ -20,7 +20,7 @@ const Banner = () => {
                     <h1 className='font-semibold text-slate-600 text-2xl mb-3'>Frontend Developer</h1>
                     <p className='px-24 text-slate-500'> I am  a React enthusiast with 6 months of coding joy. I am on a mission to create awesome user interfaces that spark joy and engagement. Let us code cool stuff together!</p>
                    <div className='flex gap-5 items-center justify-center mt-5'>
-                   <button className="btn btn-outline rounded-l-3xl rounded-r-3xl">Download Resume</button>
+                   <a href={pdf} download='redwan resume'> <button className="btn btn-outline rounded-l-3xl rounded-r-3xl">Download Resume</button></a>
                     <button className="btn  btn-neutral rounded-l-3xl rounded-r-3xl">Contact Info</button>
                    </div>
                 </div>
